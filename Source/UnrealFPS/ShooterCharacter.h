6fb1f2cf326a5e54415ad43ccaa5d0ee1cf2f42b
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-07-07 16:20:09 +0200

    implement movement binding

--- Source/UnrealFPS/ShooterCharacter.h
+++ Source/UnrealFPS/ShooterCharacter.h
@@ -26,4 +26,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void MoveForward(float AxisValue);
+	void LookUp(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+
 };
